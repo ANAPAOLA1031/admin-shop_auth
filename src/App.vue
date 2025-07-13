499a454312ae64bd012d7adf5bb7d34e255c5607
ANA <323030172@upjr.edu.mx>
2025-07-13 13:27:23 -0600

    Proyecto admin-shop

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <RouterView></RouterView>
+    <VueQueryDevtools />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+</script>
+
+<style scoped></style>
---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Header Navbar -->
+  <TopMenu />
+
+  <!-- RouterView   -->
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
+
+<style scoped></style>
